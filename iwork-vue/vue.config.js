e7202ee8da66db56dd7d9ed82bb313bab32b7b66
s16201502 <562464454@qq.com>
2019-10-25 09:52:32 +0800

    vue-cli 3.0脚手架第一次搭建

---
+++ iwork-vue/vue.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    publicPath: '/',//publicPath取代了baseUrl
+    outputDir: 'dist',
+    lintOnSave: true,
+    runtimeCompiler: true, //关键点在这  原来的 Compiler 换成了 runtimeCompiler
+    // 调整内部的 webpack 配置。
+    // 查阅 https://github.com/vuejs/vue-doc-zh-cn/vue-cli/webpack.md
+    chainWebpack: () => {},
+    configureWebpack: () => {},
+    // 配置 webpack-dev-server 行为。
+    devServer: {
+      open: process.platform === 'darwin',
+      host: '127.0.0.1',
+      port: 8081,
+      https: false,
+      hotOnly: false,
+      // 查阅 https://github.com/vuejs/vue-doc-zh-cn/vue-cli/cli-service.md#配置代理
+      proxy: null, // string | Object
+      before: app => {}
+    }
+  }
+
